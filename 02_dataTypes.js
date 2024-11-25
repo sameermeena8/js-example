19f8198e61536e3a454fc10d84000829bf3b46d8
sameer <128344244+sameermeena8@users.noreply.github.com>
2024-11-25 09:40:28 +0000

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hii soljier")  // we are using nodejs, not browser
+
+console.log(3 + 
+    3) // code readbility should br high
+
+console.log("seenu")
+
+let name = "soljier"
+let age = 22
+let isLoggedIn = true
+let number;
+let state = null
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique    
+
+//object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
