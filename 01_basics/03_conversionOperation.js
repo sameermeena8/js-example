516aaa785ca229115892220e5cec8b53e52fcf32
sameer <128344244+sameermeena8@users.noreply.github.com>
2024-11-25 13:11:06 +0000

    number conversion confusing

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 21
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************ Operations *********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " soljier"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study 
+// tc39.es.com
