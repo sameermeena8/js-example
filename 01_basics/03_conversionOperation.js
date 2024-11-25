dd3f2a822a55e48786d818240bf21307faee6838
sameer <128344244+sameermeena8@users.noreply.github.com>
2024-11-25 10:37:13 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = 22
+let score = "soljier"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "22" => 22
+// "22abc" => NaN
+// true => 1; false => 0;
+// "soljier" => NaN 
+
+let isLoggedIn = "soljier"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//  1 => true; 0> false 
+// "" => false
+// "soljier" => true 
+
+let someNumber = 21
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
